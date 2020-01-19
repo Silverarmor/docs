0a0f5a254d245ba64d763d0b1ce114d0cc7eafb6
Tobias <Suchty112@users.noreply.github.com>
2020-01-19 14:59:11 +0100

    🚀 [Eventmission][Core] Add Something

--- lssm-v3.user.js
+++ lssm-v3.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LSS Manager v3 Autoupdater
-// @version      3.3.2
+// @version      3.3.3
 // @description  Das Tool für LSS
 // @author       lost | northdegree | mausmajor | dlrg-dominik | mrweezle | sanni | jan
 // @homepage     https://lss-manager.de
@@ -26,6 +26,14 @@
 // @include      *://www.operateur112.fr/*
 // @include      *://dispetcher112.ru/*
 // @include      *://www.dispetcher112.ru/*
+// @include      *://alarmcentral-spil.dk/*
+// @include      *://www.alarmcentral-spil.dk/*
+// @include      *://nodsentralspillet.com/*
+// @include      *://www.nodsentralspillet.com/*
+// @include      *://operacni-stredisko.cz/*
+// @include      *://www.operacni-stredisko.cz/*
+// @include      *://kontrol-merkezi-112.com/*
+// @include      *://www.kontrol-merkezi-112.com/*
 // @updateURL    https://github.com/LSS-Manager/lss-manager-v3/raw/master/lssm-v3.user.js
 // @grant        none
 // @run          document-start
